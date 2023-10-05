e60bf7f060d8e4c69123d0e8245d1a57c08cc6d4
katals <juanca20042004@hotmail.com>
2023-10-05 16:59:59 -0500

    base de datos agregada al código

--- app/src/main/java/com/example/emergencyapp/MainActivity.java
+++ app/src/main/java/com/example/emergencyapp/MainActivity.java
@@ -5,8 +5,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,6 +19,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public Connection connectionDB(){
+        Connection cnn = null;
+
+        try{
+            StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+            StrictMode.setThreadPolicy(policy);
+            Class.forName("net.sourceforge.jtds.jdbc.Driver");
+            cnn = DriverManager.getConnection("jdbc:jtds:sqlserver://192.168.0.11; databaseName=EmergencyDB; integratedSecurity=true");
+        }
+        catch (Exception e){
+            Toast.makeText(getApplicationContext(), e.getMessage(), Toast.LENGTH_SHORT).show();
+        }
+        return cnn;
     }
     public void askHelpButton(View view){
         setContentView(R.layout.activity_questionary);
--- app/src/main/java/com/example/emergencyapp/models/IGoogleSheets.java
+++
@@ -1,9 +0,0 @@
-package com.example.emergencyapp.models;
-
-public interface IGoogleSheets {
-    @GET
-    Call<String> getPeople(@Url String url);
-
-    @POST("exec")
-    Call<String> getStringRequestBody(@Body String body);
-}
